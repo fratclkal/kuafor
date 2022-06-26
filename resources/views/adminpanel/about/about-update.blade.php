1472fa0f7d80cc43a67b0a27f7b6f68b28a2ecc2
Mehmet <97066131+Mehmet1Ozdemir@users.noreply.github.com>
2022-06-26 15:53:19 +0300

    Admin Panelde Hakkımızda Sayfasının Güncelleme Sayfasının Tasarımı

---
+++ resources/views/adminpanel/about/about-update.blade.php
@@ -0,0 +1,51 @@
+@extends('adminpanel.app')
+@section('content')
+
+    <div class="col-md-12 col-12">
+        <div class="card">
+            <div class="card-header">
+                <h4 class="card-title">Hakkımızda Güncelleme</h4>
+            </div>
+            <div class="card-content">
+                <div class="card-body">
+                    <form class="form form-horizontal" method="POST" action="">
+                        @csrf
+                        <div class="form-body">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <label>Başlık : </label>
+                                </div>
+                                <div class="col-md-12 form-group">
+                                    <input type="text" id="first-name" class="form-control" name="title" placeholder="Başlık..">
+                                </div>
+                                <div class="col-md-4">
+                                    <label>İçerik : </label>
+                                </div>
+                                <div class="col-md-12 form-group">
+                                    <textarea name="content" id="ck" cols="105" rows="10"></textarea>
+                                </div>
+
+                                <div class="col-sm-12 d-flex justify-content-end">
+                                    <button type="submit" class="btn btn-primary me-1 mb-1">Kaydet</button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        const textarea = document.querySelector( '#ck' );
+
+        ClassicEditor
+            .create( textarea )
+            .then( editor => { window.editor = editor } );
+
+        document.getElementById( 'submit' ).onclick = () => {
+            textarea.value = editor.getData();
+        }
+    </script>
+
+@endsection
--- resources/views/adminpanel/about/about.blade.php
+++ resources/views/adminpanel/about/about.blade.php
@@ -6,47 +6,7 @@
 
 @section('content')
 
-    <div class="col-md-12 col-12">
-        <div class="card">
-            <div class="card-header">
-                <h4 class="card-title">HAKKIMIZDA</h4>
-            </div>
-            <div class="card-content">
-                <div class="card-body">
-                    <form class="form form-horizontal" method="POST" action="{{route('createAbout')}}">
-                        @csrf
-                        <div class="form-body">
-                            <div class="row">
-                                <div class="col-md-4">
-                                    <label>Başlık : </label>
-                                </div>
-                                <div class="col-md-12 form-group">
-                                    <input type="text" id="first-name" class="form-control" name="title" placeholder="Başlık..">
-                                </div>
-                                <div class="col-md-4">
-                                    <label>İçerik : </label>
-                                </div>
-                                <!--<div class="col-md-12 form-group" >
-                                    <div id="editor">
-                                        <textare name="content" class="form-control"></textare>
-                                    </div>
 
-
-                                </div> -->
-                                <div class="col-md-12 form-group">
-                                    <textarea name="content" id="ck" cols="105" rows="10"></textarea>
-                                </div>
-
-                                <div class="col-sm-12 d-flex justify-content-end">
-                                    <button type="submit" class="btn btn-primary me-1 mb-1">Kaydet</button>
-                                </div>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
 
 
     <section class="section">
@@ -100,17 +60,7 @@
                 console.error( error );
             } );
     </script>-->
-    <script>
-        const textarea = document.querySelector( '#ck' );
 
-        ClassicEditor
-            .create( textarea )
-            .then( editor => { window.editor = editor } );
-
-        document.getElementById( 'submit' ).onclick = () => {
-            textarea.value = editor.getData();
-        }
-    </script>
 
 
 @endsection
