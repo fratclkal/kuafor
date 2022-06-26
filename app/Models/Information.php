30bc6750f11e1170033e3d930fcd518c7acf05ae
alpeercetinnnn <Kh1T3eor1>
2022-06-26 18:32:41 +0300

    Projenin Veri Tabanı Oluşturulması (Migration) Projenin Veri Tabanı Oluşturulması (Model)

---
+++ app/Models/Information.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Information extends Model
+{
+    use HasFactory;
+
+    protected $table = "informations";
+
+    protected $fillable = [
+      'name_surname',
+      'phone_num'
+    ];
+
+    protected $guarded = [
+      'id'
+    ];
+}
